dbfcf4a8d4de05b93f76680257fa5faec453fd34
BuseYalcin <buseylcin5@gmail.com>
2022-01-31 21:22:06 +0300

    Jwt hataları düzenlendi.

--- src/main/java/com/works/services/UserService.java
+++ src/main/java/com/works/services/UserService.java
@@ -1,9 +1,5 @@
 package com.works.services;
-
 import com.works.entities.Users;
-import com.works.repositories.UserRepository;
-import org.springframework.stereotype.Service;
-
 import java.util.Collection;
 
 public interface  UserService{
